9d9009e095f1aa29e0d64e3ab3fc19ca299019c6
Kerem Ayseli <46223333+KeremAyseli@users.noreply.github.com>
2021-08-25 16:50:02 +0300

    delete commit

--- main/java/com/themessagger/Message/MessaggeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.themessagger.Message;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessaggeRepository extends MongoRepository<Message, String> {
-    
-}
